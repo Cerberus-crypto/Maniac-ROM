53431ad93d6ced8be56f82c235cf7a42dfbe9547
test <ricci206@virgilio.it>
2026-06-25 14:58:36 +0200

    Fix PetService

--- platform/exynos990/debloat.sh
+++ platform/exynos990/debloat.sh
@@ -23,16 +23,13 @@
 # Camera SDK
 SYSTEM_DEBLOAT+="
 system/etc/default-permissions/default-permissions-com.samsung.android.globalpostprocmgr.xml
-system/etc/default-permissions/default-permissions-com.samsung.petservice.xml
 system/etc/permissions/cameraservice.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.globalpostprocmgr.xml
-system/etc/permissions/privapp-permissions-com.samsung.petservice.xml
 system/etc/permissions/sec_camerax_impl.xml
 system/etc/permissions/sec_camerax_service.xml
 system/framework/sec_camerax_impl.jar
 system/framework/scamera_sep.jar
 system/priv-app/GlobalPostProcMgr
-system/priv-app/PetService
 system/priv-app/SCameraSDKService
 system/priv-app/sec_camerax_service
 "
