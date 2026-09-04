16e2bc7a5c227664df2c21b8873a94a919c21226
samsunggithub <140867325+samsunggithub@users.noreply.github.com>
2026-09-04 14:47:01 +0800

    Update customize.sh

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -8,9 +8,9 @@ LOG_STEP_IN "- Enabling BSOH in SecSettings"
 DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
 
 FTP="
-system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/deviceinfo/batteryinfo/BatteryRegulatoryPreferenceController.smali
-system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/deviceinfo/batteryinfo/SecBatteryFirstUseDatePreferenceController.smali
-system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/deviceinfo/batteryinfo/SecBatteryInfoFragment.smali
+system/priv-app/SecSettings/SecSettings.apk/smali_classes5/com/samsung/android/settings/deviceinfo/batteryinfo/BatteryRegulatoryPreferenceController.smali
+system/priv-app/SecSettings/SecSettings.apk/smali_classes5/com/samsung/android/settings/deviceinfo/batteryinfo/SecBatteryFirstUseDatePreferenceController.smali
+system/priv-app/SecSettings/SecSettings.apk/smali_classes5/com/samsung/android/settings/deviceinfo/batteryinfo/SecBatteryInfoFragment.smali
 "
 for f in $FTP; do
     sed -i "s/SM-A236B/SM-S908B/g" "$APKTOOL_DIR/$f"
